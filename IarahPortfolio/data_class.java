f34c9d93db5155bd331a2a026276bd9916ca1564
ia_mae <84659066+iamae10@users.noreply.github.com>
2023-06-27 03:37:18 +0800

    Create data_class.java

---
+++ IarahPortfolio/data_class.java
@@ -0,0 +1,22 @@
+
+ 
+public class data_class {
+    
+	private String password;
+
+	
+	public data_class(String password) {
+		this.password = password;
+	}
+
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+}
