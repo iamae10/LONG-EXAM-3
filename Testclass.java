f9d7984e274fb8ff654808fa036adb6e7cb1ebb6
ia_mae <84659066+iamae10@users.noreply.github.com>
2023-06-27 09:40:46 +0800

    Create Testclass.java

---
+++ Testclass.java
@@ -0,0 +1,90 @@
+import org.junit.jupiter.api.Test;
+
+public class Testclass {
+  
+	
+	@Test
+	public void testmyPortfolioFrame() {
+		mainFrame class1 = new mainFrame();
+		//test the mainFrame class
+	}
+	//Jaellen's portfolio classes
+	@Test
+	public void testMain() {
+		Main class2 = new Main();
+	}
+	@Test
+	public void testAboutMe() {
+		AboutMe class3 = new AboutMe();
+	}
+	@Test
+	public void testContacts() {
+		Contacts class4 = new Contacts();
+	}
+	@Test
+	public void testProfile() {
+		Profile class5 = new Profile();
+	}
+	@Test
+	public void testWorks() {
+		Works class6 = new Works();
+	}
+	@Test
+	public void testNextWorks() {
+		NextWorks class7 = new NextWorks();
+	}
+	//Mykie's portfolio classes
+	@Test
+	public void testPorfolioHome() {
+		NextWorks class8 = new NextWorks();
+	}
+	@Test
+	public void testPortfolioBase() {
+		PortfolioBase class9 = new PortfolioBase();
+	}
+	@Test
+	public void testPortfolioMyCreations() {
+		PortfolioMyCreations class10 = new PortfolioMyCreations();
+	}
+	@Test
+	public void testPortfolioMySkills() {
+		PortfolioMySkills class11 = new PortfolioMySkills();
+	}
+	@Test
+	public void testPortfolioAboutMe() {
+		PortfolioMySkills class12 = new PortfolioMySkills();
+	}
+	//iarah's portfolio classes
+	@Test
+	public void testPortfolio() {
+		Portfolio class13= new Portfolio();
+	}
+	@Test
+	public void testmyPortfolio() {
+		Portfolio class14= new Portfolio();
+	}
+	@Test
+	public void testContact() {
+		contact class15 = new contact();
+	}
+	@Test
+	public void testAbout() {
+		about class16 = new about();
+	}
+	@Test
+	public void testWhatIDo() {
+		WhatIDo class17 = new WhatIDo();
+	}
+	@Test
+	public void testPhotos() {
+		photos class18 = new photos();
+	}
+	@Test
+	public void testArts() {
+		arts class19 = new arts();
+	}
+	@Test
+	public void testPortfolioFrame() {
+		PortfolioFrame class20 = new PortfolioFrame();
+	}
+}
